8f047eade7abc0dfa9fcf50e9b959c356d826587
bwaah1 <chykivan@gmail.com>
2021-10-21 12:23:47 +0300

    Add project files.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int R;
+			double x = 5;
+			double n = 2;
+
+			R = (1. * x * x - 2 * x * x * n) / (2. * n + 1);
+			Assert::AreEqual(R, -15);
+		}
+	};
+}
